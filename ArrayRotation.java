212ee6f43f2f70d97baecfeb7dee661445a22ef1
Karthikeyan Mohan <karthik@Karthikeyans-MacBook-Pro.local>
2016-08-25 15:30:43 -0700

    Rotates the array for a given number of times to the left.
    Input format: array length rotation times
    Each line contains array element.
    
    Final output will be printed with space.

---
+++ ArrayRotation.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class ArrayRotation {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner scanner = new Scanner(System.in);
+        String values = scanner.nextLine();
+        String[] arraySizeAndRotation = values.split(" ");
+        int[] a = new int[Integer.parseInt(arraySizeAndRotation[0])];
+        for (int i=0; i< Integer.parseInt(arraySizeAndRotation[0]); i++) {
+            a[i] = Integer.parseInt(scanner.next());
+        }
+        
+        for(int i =0 ; i< Integer.parseInt(arraySizeAndRotation[1]); i++){
+            a = rotate(a);
+        }
+    
+        
+        for( int i =0;i < a.length; i++){
+            System.out.print(a[i]);
+            System.out.print(" ");
+        }
+        
+    }
+    
+    public static int[] rotate(int[] a){
+        int firstElement = a[0];
+        for ( int i = 0; i< a.length; i++){
+            if(i == a.length -1){
+                a[a.length - 1] = firstElement;
+            }
+            else {
+                a[i] = a[i+1];
+            }
+            
+        }
+        return a;
+    }
+}
