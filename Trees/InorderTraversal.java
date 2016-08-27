11ab6f64c537ebc12e805e42dee23b1c96fe807c
Karthikeyan Mohan <mkarthik90@gmail.com>
2016-08-27 15:13:14 -0700

    Added inorder traversal

---
+++ Trees/InorderTraversal.java
@@ -0,0 +1,23 @@
+class Node {
+    int data;
+    Node left;
+    Node right;
+}
+
+class InorderTraversal {
+
+    void inOrder(Node root) {
+        
+        if(root.left != null) {
+            inOrder(root.left);        
+           
+        }
+        
+         System.out.print(root.data+" ");
+        
+        if(root.right != null) {
+            inOrder(root.right);
+        }
+    }
+
+}
