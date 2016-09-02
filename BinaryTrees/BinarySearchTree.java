ee201ac59c96c01ae9afd4933f6694a89d80b322
Karthikeyan Mohan <mkarthik90@gmail.com>
2016-09-01 22:52:08 -0700

    Added algo for getting width in each level in tree

--- BinaryTrees/BinarySearchTree.java
+++ BinaryTrees/BinarySearchTree.java
@@ -1,11 +1,13 @@
-class BinaryTree{
+class BinarySearchTree {
 	
+	public static int[] count = new int[10];
+
 	/*
 	* Finds the height or depth of the binary tree.
 	*/
 	public static int height(Node root) {
 		if(root == null) {
-			return null;
+			return 0;
 		}
 		else {
 			int lDepth = height(root.left);
@@ -19,7 +21,7 @@ class BinaryTree{
 		}
 	}
 
-	public static void insert(Node root, int val) {
+	public static Node insert(Node root, int val) {
 		Node prev = null;
 		Node tempRoot = null;
 		while(root != null) {
@@ -50,13 +52,37 @@ class BinaryTree{
 		}
 		else {
 			inOrder(root.left);
-			System.out.print(root.data);
+			System.out.print(root.data+" ");
 			inOrder(root.right);
 
 		}
 	}
 
+	/*
+	* Finding width of each level in a binary tree
+	*/
+	public static void getWidth(Node root, int level) {
+		// If the node is null, then that wont add to that level
+		// Similar recurssion as preorder
+		if(root != null) {
+			count[level]++;
+			level++;
+			getWidth(root.left, level);
+			getWidth(root.right, level);
+		}
+	}
+
+
+
 	public static void main(String arr[]) {
+		Node root = insert(null, 50);
+		insert(root, 30);
+		insert(root, 20);
+		insert(root, 40);
+		insert(root, 90);
+		insert(root, 60);
+		insert(root, 110);
 		
+		inOrder(root);
 	}
 }
