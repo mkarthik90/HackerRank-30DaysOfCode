ed4e110ddcb4a29b007d92498b2d1dab70e823b2
Karthikeyan Mohan <mkarthik90@gmail.com>
2016-10-03 15:58:42 -0700

    Added code for comparing if two binary trees are same.

---
+++ BinaryTrees/SameTree.java
@@ -0,0 +1,20 @@
+/**
+ * Check if two binary tree is same
+**/
+class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        
+        if(p == null && q == null) {
+            return true;
+        }
+        
+        if(p == null || q == null) {
+            return false;
+        }
+        
+        if(p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
