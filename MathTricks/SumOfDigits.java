68c6a9b2b17173b325cae5a44e4176381e77a75d
Karthikeyan Mohan <mkarthik90@gmail.com>
2016-10-09 19:59:27 -0700

    Added code for finding sum of digits of a number
    Basic idea is to do a Mod 9.

---
+++ MathTricks/SumOfDigits.java
@@ -0,0 +1,10 @@
+class SumOfDigits {
+    public static void main(String[] arr) {
+        int n = 9991;
+        if(n%9 == 0) {
+            System.out.println(9);
+        } else {
+            System.out.println(n%9);
+        }
+    }
+}
